bc9c47d28531daa20f1d47734d5850f10b4a55ee
Jose daniel Tello moscoso <jtello-m@c2r3s2.42madrid.com>
2019-12-20 19:20:56 +0100

    42 NICE

---
+++ JTC01/ex05/ft_putstr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtello-m <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/08 17:31:30 by jtello-m          #+#    #+#             */
+/*   Updated: 2019/09/09 16:10:28 by jtello-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int contador;
+	int longitud;
+
+	contador = 0;
+	while (str[contador] != '\0')
+	{
+		contador++;
+	}
+	longitud = contador;
+	contador = 0;
+	while (contador < longitud)
+	{
+		write(1, &str[contador], 1);
+		contador++;
+	}
+}
---
+++ JTC01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtello-m <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/09 11:09:20 by jtello-m          #+#    #+#             */
+/*   Updated: 2019/09/09 16:31:35 by jtello-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int i;
+	int j;
+	int aux;
+
+	i = 0;
+	while (i < size)
+	{
+		j = 0;
+		while (j < size - 1 - i)
+		{
+			if (tab[j] > tab[j + 1])
+			{
+				aux = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = aux;
+			}
+			j++;
+		}
+		i++;
+	}
+}
